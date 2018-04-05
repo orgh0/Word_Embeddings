a5159e39cd0946ae95d5a7d0ac335387de206ecb
arghyatiger <arghyatiger@gmail.com>
2018-04-05 11:07:33 +0530

    Uploaded basic script

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Word Embeddings
+
+## What are word embeddings ?
+
+Word embedding is the collective name for a set of language modeling and feature learning techniques in natural language processing (NLP) where words or phrases from the vocabulary are mapped to vectors of real numbers. Conceptually it involves a mathematical embedding from a space with one dimension per word to a continuous vector space with much lower dimension.
+
+## About Repo
+
+This repository contains scripts for playing around with the common pretrained work embedding vectors like `Fasttext` , `Glove`
+
---
+++ test.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+from gensim.models import KeyedVectors
+
+
+find_similar_to = 'dog'
+positive_instance = ['king', 'queen']
+negative_instance = ['male']
+
+print("Model loading started")
+en_model = KeyedVectors.load_word2vec_format('../wiki.en/wiki.en.vec')
+print("Model loading ended")
+
+words = []
+for word in en_model.vocab:
+    words.append(word)
+
+
+for similar_word in en_model.similar_by_word(find_similar_to):
+    print("Word: {0}, Similarity: {1:.2f}".format(similar_word[0], similar_word[1]))
+
+
+for resultant_word in en_model.most_similar(positive=positive_instance, negative=negative_instance topn=1):
+    print("Word : {0} , Similarity: {1:.2f}".format(resultant_word[0], resultant_word[1]))
+
